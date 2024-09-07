455983dcb76af44793239a8c02a1136963733092
kamranBaloch1 <kamranbalochhh18@gmail.com>
2024-09-07 14:40:07 -0700

    first commit

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function loading() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
